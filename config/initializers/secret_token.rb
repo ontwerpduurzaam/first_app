601ffc7298dc13fb343ded3cb3584a8465165921
info@ontwerpduurzaam.nl <info@ontwerpduurzaam.nl>
2013-12-03 21:44:11 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '4fd4ac32f91d97943771fdbf0568a35f82db15b33129d524761c4e07b52e06dc754b45eb68cb389ddb9eaba84e8badf87595f5efa5b6705b02e5b1fd279c3918'
